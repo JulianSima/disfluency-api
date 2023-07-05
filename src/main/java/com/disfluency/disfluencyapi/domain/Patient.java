03561e2e4ce1c61ac7c584f7cb50219b1e4f4298
MartinezAlexander <alexandercristianmartinez@gmail.com>
2023-07-04 23:24:15 -0300

    Changed patientController to english language

--- src/main/java/com/disfluency/disfluencyapi/domain/Patient.java
+++ src/main/java/com/disfluency/disfluencyapi/domain/Patient.java
@@ -1,10 +1,17 @@
 package com.disfluency.disfluencyapi.domain;
 
+import com.disfluency.disfluencyapi.dto.NewPatientDTO;
+import com.disfluency.disfluencyapi.dto.PatientDTO;
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@Builder
 public class Patient implements UserRole {
     //TOOD: falta User
     private String id;
@@ -17,4 +24,23 @@ public class Patient implements UserRole {
     private List<Session> therapySession;
     private List<ExerciseAssignment> exerciseAssignments;
     private List<FormAssignment> formAssignments;
+
+    public int getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+    public PatientDTO toDTO() {
+        return new PatientDTO(id, name, lastName, getAge());
+    }
+
+    public static Patient newPatient(NewPatientDTO newPatientDTO, String therapistId) {
+        return Patient.builder()
+                .name(newPatientDTO.name())
+                .lastName(newPatientDTO.lastName())
+                .dateOfBirth(newPatientDTO.birthday())
+                .exerciseAssignments(new ArrayList<>())
+                .formAssignments(new ArrayList<>())
+                //.therapistId(therapistId)
+                .build();
+    }
 }

dc09d2b52918af70f0cbd1b4da55dd4c47a94e47
AgustinCragno <cragno.agustin@gmail.com>
2023-09-05 20:23:59 -0300

    Fixed s3 yaml value read

--- src/main/java/com/disfluency/disfluencyapi/service/aws/S3Service.java
+++ src/main/java/com/disfluency/disfluencyapi/service/aws/S3Service.java
@@ -3,6 +3,7 @@ package com.disfluency.disfluencyapi.service.aws;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
@@ -14,12 +15,20 @@ public class S3Service {
 
     private final AmazonS3 amazonS3Client;
 
-    public static final Integer PRE_SIGNED_UPLOAD_EXPIRATION = Integer.parseInt("${amazon.aws.uploadExpiration}");
-    public static final Integer PRE_SIGNED_GET_EXPIRATION = Integer.parseInt("${amazon.aws.getExpiration}");
+    @Value("${amazon.aws.uploadExpiration}")
+    public static Integer PRE_SIGNED_UPLOAD_EXPIRATION;
 
-    public static final String S3_BASE_URL = "${amazon.aws.s3BaseUrl}";
-    public static final String S3_BUCKET = "${amazon.aws.s3Bucket}";
-    public static final String S3_UPLOAD_FOLDER = "${amazon.aws.s3UploadFolder}";
+    @Value("${amazon.aws.getExpiration}")
+    public static Integer PRE_SIGNED_GET_EXPIRATION;
+
+    @Value("${amazon.aws.s3BaseUrl}")
+    public static String S3_BASE_URL;
+
+    @Value("${amazon.aws.s3Bucket}")
+    public static String S3_BUCKET;
+
+    @Value("${amazon.aws.s3UploadFolder}")
+    public static String S3_UPLOAD_FOLDER;
 
     public String generatePreSignedUrl(String filePath, String bucketName, HttpMethod httpMethod, int expirationMinutes) {
         var calendar = Calendar.getInstance();
